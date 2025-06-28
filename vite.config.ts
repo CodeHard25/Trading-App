59800c4eff3274975bd223986dba05fc66830cc3
CodeHard25 <hardiktyagi007@gmail.com>
2025-06-28 21:02:40 +0530

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Trading-App/index.html",
+  base: "/Trading-App/",
   server: {
     host: "::",
     port: 8080,
