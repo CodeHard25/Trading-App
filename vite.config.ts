e010172fa2e9c47c4ad3d1df47c7c62ca2866ef0
CodeHard25 <hardiktyagi007@gmail.com>
2025-06-28 22:03:33 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Trading-App/dashboard/",
+  base: "/Trading-App/dist/assets/",
   server: {
     host: "::",
     port: 8080,
