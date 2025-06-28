0eb4176a231143363c0bb1c3165fa31b4d09ae68
CodeHard25 <hardiktyagi007@gmail.com>
2025-06-28 21:40:53 +0530

    One Last Push

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Trading-App/",
+  base: "/Trading-App/dashborard/",
   server: {
     host: "::",
     port: 8080,
